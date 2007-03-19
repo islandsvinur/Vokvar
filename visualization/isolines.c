6653442509c60de536b4f4c215dd85e4d43e9b44
Christian Luijten <christian@luijten.org>
2007-03-19 21:41:29 +0000

    Small fix (math.h loaded)

--- visualization/isolines.c
+++ visualization/isolines.c
@@ -1,6 +1,7 @@
 #include "isolines.h"
 #include "../simulation.h"
 #include <rfftw.h>
+#include <math.h>
 
 #define MAX(a, b)  (((a) > (b)) ? (a) : (b))
 #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
